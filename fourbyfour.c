3e7172fd625d278ca30556e3be0f912d46aa7ade
psysr4 <psysr4@nottingham.ac.uk>
2025-02-25 22:10:09 +0000

    16 grid output

--- fourbyfour.c
+++ fourbyfour.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void outputGrid(int* arr);
+
 int main(int argc, char *argv[]) { 
     int* array = calloc(16, sizeof(int));
     outputGrid(array);
     free(array);
+    printf("\n");
     return 0;
 }
 
 
 void outputGrid(int *arr) {
-    for(int i = 0; i < 16; i++) {
-        if((i%2)==0) {
-            printf("|");
-        }
-        if((i%4)==0) {
-            printf("\n");
-        }
-        printf("%d", arr[i]);
+    for(int i = 0; i < 16; i+=4) {
+        printf("|%d%d|%d%d|\n", arr[i], arr[i+1], arr[i+2], arr[i+3]);
     }
     return;
 }
